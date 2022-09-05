4e434217795f220d4353737219ff3d664e987677
houxiaoyi <xiaoyihou@outlook.com>
2022-09-05 13:42:12 +0800

    vue3 init

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,34 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import { defineComponent } from 'vue'
+import { ElConfigProvider } from 'element-plus'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
-nav {
+export default defineComponent({
+  components: {
+    [ElConfigProvider.name]: ElConfigProvider,
+  },
+  data() {
+    return {
+      locale: zhCn,
+    }
+  },
+})
+</script>
+
+<style lang="scss">
+html,
+body,
+#app {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/router/modules/home.js
@@ -0,0 +1,26 @@
+// home.js
+const Layout = () => import('@/layout/index.vue')
+const Home = () => import('@/views/home/index.vue')
+
+export default [
+  {
+    path: '/home',
+    component: Layout,
+    name: 'Dashboard',
+    meta: {
+      title: '工作台',
+    },
+    icon: 'home',
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: Home,
+        meta: {
+          title: '首页',
+          affix: true,
+        },
+      },
+    ],
+  },
+]
