1f85db38df27b88bba931efd1f38ab687474237c
houxiaoyi <xiaoyihou@outlook.com>
2023-07-10 18:03:52 +0800

    页面跳转

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <el-card class="box-card-process-list" :body-style="{ padding: '10px',height: '100%'}">
+  <el-card
+    class="box-card-process-list"
+    :body-style="{ padding: '10px', height: '100%' }"
+  >
     <el-tabs
       v-model="home.activeName"
       class="flowable-tabs"
@@ -14,8 +17,16 @@
           :data="home.processList"
         >
           <vxe-column type="seq" width="60"></vxe-column>
-          <vxe-column field="name" title="Name" show-overflow></vxe-column>
-          <vxe-column field="address" title="Address" width="60"></vxe-column>
+          <vxe-column field="name" title="名称" show-overflow></vxe-column>
+          <vxe-column title="操作" width="60" show-overflow>
+            <template #default="{ row }">
+              <vxe-button
+                type="text"
+                icon="vxe-icon-edit"
+                @click="startProcess(row)"
+              ></vxe-button>
+            </template>
+          </vxe-column>
         </vxe-table>
         <vxe-pager
           size="mini"
@@ -35,38 +46,51 @@
   </el-card>
 </template>
 
-<script lang="ts" setup>
-import { onMounted, reactive } from "vue";
+<script lang="ts">
+import { defineComponent, onMounted, reactive } from "vue";
 import type { TabsPaneContext } from "element-plus";
 import { obtainProcessList } from "@/api/flowable";
+import { useRouter } from "vue-router";
 
-const home = reactive({
-  activeName: "todo",
-  processList: [],
-  processPage: {
-    currentPage: 0,
-    pageSize: 10,
-    total: 0,
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+
+    const home = reactive({
+      activeName: "todo",
+      processList: [],
+      processPage: {
+        currentPage: 0,
+        pageSize: 10,
+        total: 0,
+      },
+      todoList: [],
+      handledList: [],
+      launchList: [],
+    });
+
+    const tabHandleClick = (tab: TabsPaneContext, event: Event) => {
+      console.log(tab, event);
+    };
+
+    onMounted(() => {
+      loadProcessList();
+    });
+
+    const loadProcessList = () => {
+      obtainProcessList(home.processPage).then((res: any) => {
+        home.processList = res.data;
+        home.processPage.total = res.total;
+      });
+    };
+
+    const startProcess = (row: any) => {
+      router.push({ path: row.processDisposePath });
+    };
+
+    return { home, tabHandleClick, loadProcessList, startProcess };
   },
-  todoList: [],
-  handledList: [],
-  launchList: [],
 });
-
-const tabHandleClick = (tab: TabsPaneContext, event: Event) => {
-  console.log(tab, event);
-};
-
-onMounted(() => {
-  loadProcessList();
-});
-
-const loadProcessList = () => {
-  obtainProcessList(home.processPage).then((res: any) => {
-    home.processList = res.data;
-    home.processPage.total = res.total;
-  });
-};
 </script>
 
 <style lang="scss" scoped>
